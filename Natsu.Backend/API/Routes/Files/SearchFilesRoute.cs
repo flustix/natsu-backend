79762965bc09804e77258cca97689567e17d4c16
flustix <me@flux.moe>
2024-08-10 03:21:35 +0200

    ah

---
+++ Natsu.Backend/API/Routes/Files/SearchFilesRoute.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using Natsu.Backend.API.Components;
+using Natsu.Backend.Database.Helpers;
+using Natsu.Backend.Models;
+
+namespace Natsu.Backend.API.Routes.Files;
+
+public class SearchFilesRoute : INatsuAPIRoute
+{
+    public string RoutePath => "/files";
+    public HttpMethod Method => HttpMethod.Get;
+
+    public async Task Handle(NatsuAPIInteraction interaction)
+    {
+        var query = interaction.GetStringQuery("q") ?? "";
+        var offset = interaction.GetIntQuery("o") ?? 0;
+
+        var all = TaggedFileHelper.All;
+        all.RemoveAll(x => !matches(x, query));
+
+        var files = all.Skip(offset).Take(50);
+        await interaction.Reply(HttpStatusCode.OK, files);
+    }
+
+    private static bool matches(TaggedFile file, string query)
+    {
+        var words = query.Split(' ');
+
+        var match = words.All(w => file.Description.Contains(w));
+        return match;
+    }
+}

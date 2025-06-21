8c1cb060663df8799b804c5393203c1843bc6278
flustix <me@flux.moe>
2025-06-21 02:40:10 +0200

    property filters

--- Natsu.Backend/Utils/SearchFilter.cs
+++ Natsu.Backend/Utils/SearchFilter.cs
@@ -28,10 +28,13 @@ public partial class SearchFilter<T>
                 text.Add(tokens[i]);
         }
 
-        // var propEx = props.Count > 0 ? null : null;
+        var propEx = props.Count > 0 ? buildProps(props) : null;
         var textEx = text.Count > 0 ? buildText(text) : null;
 
-        expr = textEx ?? (x => true);
+        if (propEx != null && textEx != null)
+            expr = Expression.Lambda<Func<T, bool>>(Expression.AndAlso(propEx, textEx), propEx.Parameters);
+        else
+            expr = propEx ?? textEx ?? (x => true);
     }
 
     #region Simple Text
@@ -70,6 +73,75 @@ public partial class SearchFilter<T>
         }
 
         return finalExpr == null ? x => false : Expression.Lambda<Func<T, bool>>(finalExpr, param);
+    }
+
+    #endregion
+
+    #region Properties
+
+    private Expression<Func<T, bool>> buildProps(List<string> tokens)
+    {
+        var param = Expression.Parameter(typeof(T), "x");
+        var props = typeof(T).GetProperties().Where(p => p.GetCustomAttribute<SearchableAttribute>() != null)
+                             .Where(p => p.PropertyType == typeof(string))
+                             .ToDictionary(x => x.GetCustomAttribute<SearchableAttribute>()!.Key.ToLowerInvariant(), x => x);
+
+        if (props.Count == 0)
+            return x => false;
+
+        var groups = groupTokens(tokens);
+        Expression? finalExpr = null;
+
+        foreach (var (key, op, val) in groups)
+        {
+            if (!props.TryGetValue(key.ToLowerInvariant(), out var info))
+                return x => false;
+
+            var searchText = val.Trim('*');
+            var method = getStringComparisonMethod(val.StartsWith('*'), val.EndsWith('*'));
+
+            var valueExpr = Expression.Constant(searchText);
+            var property = Expression.Property(param, info);
+            Expression condition = Expression.Call(property, method, valueExpr);
+
+            if (op == "!=")
+                condition = Expression.Not(condition);
+
+            finalExpr = finalExpr == null ? condition : Expression.AndAlso(finalExpr, condition);
+        }
+
+        return finalExpr == null ? x => false : Expression.Lambda<Func<T, bool>>(finalExpr, param);
+    }
+
+    private List<(string, string, string)> groupTokens(List<string> tokens)
+    {
+        var list = new List<(string, string, string)>();
+
+        string key = string.Empty;
+        string op = string.Empty;
+
+        for (var i = 0; i < tokens.Count; i++)
+        {
+            var idx = i % 3;
+            var t = tokens[i];
+
+            switch (idx)
+            {
+                case 0:
+                    key = t;
+                    break;
+
+                case 1:
+                    op = t;
+                    break;
+
+                case 2:
+                    list.Add((key, op, t));
+                    break;
+            }
+        }
+
+        return list;
     }
 
     #endregion
